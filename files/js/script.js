002f54d63d16ea0d1d934df26c76a02b418f17c9
Andrew Ross <andrew.ross4224@gmail.com>
2023-09-26 20:54:56 -0400

    setup basic fetch for weather api in js file

--- files/js/script.js
+++ files/js/script.js
@@ -1,1 +1,12 @@
 console.log('hello world')
+// denver lat and long
+var weatherurl = 'https://api.openweathermap.org/data/2.5/forecast?lat=39.7392&lon=104.9903&appid=1c080c27bdccf0e577c9e856b87b1d20'
+
+fetch(weatherurl)
+.then(function(response){
+    console.log(response)
+    return response.json();
+})
+.then(function(data){
+    console.log(data)
+})
