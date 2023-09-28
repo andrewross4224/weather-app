4a41701ffda1120888c2d9149212985d4ccd8672
Andrew Ross <andrew.ross4224@gmail.com>
2023-09-28 12:31:43 -0400

    set up event listener for text box and wrapped fetch calls in function to be called when button is clicked

--- files/js/script.js
+++ files/js/script.js
@@ -1,12 +1,35 @@
-console.log('hello world')
 // denver lat and long
-var weatherurl = 'https://api.openweathermap.org/data/2.5/forecast?lat=39.7392&lon=104.9903&appid=1c080c27bdccf0e577c9e856b87b1d20'
+var searchButton = document.getElementById("searchButton");
+var searchInput = document.getElementById("searchBar");
+var city;
+var lon;
+var lat;
 
-fetch(weatherurl)
-.then(function(response){
-    console.log(response)
-    return response.json();
-})
-.then(function(data){
-    console.log(data)
-})
+function checkWeather() {
+    var weatherurl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=1c080c27bdccf0e577c9e856b87b1d20';
+    fetch(weatherurl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+function getCity(event) {
+    event.preventDefault();
+    var geo = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchInput.value + '&limit=5&appid=1c080c27bdccf0e577c9e856b87b1d20';
+    fetch(geo)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            city = data[0].name;
+            lon = data[0].lon;
+            lat = data[0].lat;
+            checkWeather();
+        })
+}
+
+searchButton.addEventListener('click', getCity);
